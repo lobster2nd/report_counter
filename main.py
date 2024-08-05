f30d9e7dfe45ebed6f478e3e8d2c43f7e7b18f7a
Vladimir_T <lobster2@mail.ru>
2024-08-05 17:06:00 +0300

    Начато внедрение работы с таблицей через интерфейс

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import flet as ft
 
+from table import create_table, modify_table
+
 SCAN_CNT = 'Кол-во исследований'
 IMG_CNT = 'Кол-во снимков'
 
 
 def main(page: ft.Page):
+
+    create_table()
+
+    save_btn = ft.ElevatedButton('Сохранить')
 
     page.window_width = 650
     page.window_height = 900
@@ -12,54 +18,54 @@ def main(page: ft.Page):
     header = ft.Text(value='Количество процедур по исследованиям')
 
     chest = ft.Text(value='ОГК')
-    chest_scan_cnt = ft.TextField(value=SCAN_CNT)
-    chest_img_cnt = ft.TextField(value=IMG_CNT)
+    chest_scan_cnt = ft.TextField(label=SCAN_CNT)
+    chest_img_cnt = ft.TextField(label=IMG_CNT)
 
     limbs = ft.Text(value='Конечности')
-    limbs_scan_cnt = ft.TextField(value=SCAN_CNT)
-    limbs_img_cnt = ft.TextField(value=IMG_CNT)
+    limbs_scan_cnt = ft.TextField(label=SCAN_CNT)
+    limbs_img_cnt = ft.TextField(label=IMG_CNT)
 
-    pelvis = ft.Text(value='Таз и ТБС')
-    pelvis_scan_cnt = ft.TextField(value=SCAN_CNT)
-    pelvis_img_cnt = ft.TextField(value=IMG_CNT)
+    pelvis = ft.Text(value='Таза и тазобедренных суставов')
+    pelvis_scan_cnt = ft.TextField(label=SCAN_CNT)
+    pelvis_img_cnt = ft.TextField(label=IMG_CNT)
 
-    neck = ft.Text(value='ШОП')
-    neck_scan_cnt = ft.TextField(value=SCAN_CNT)
-    neck_img_cnt = ft.TextField(value=IMG_CNT)
+    neck = ft.Text(value='Шейные позвонки')
+    neck_scan_cnt = ft.TextField(label=SCAN_CNT)
+    neck_img_cnt = ft.TextField(label=IMG_CNT)
 
-    gop = ft.Text(value='ГОП')
-    gop_scan_cnt = ft.TextField(value=SCAN_CNT)
-    gop_img_cnt = ft.TextField(value=IMG_CNT)
+    gop = ft.Text(value='Грудные позвонки')
+    gop_scan_cnt = ft.TextField(label=SCAN_CNT)
+    gop_img_cnt = ft.TextField(label=IMG_CNT)
 
-    pkop = ft.Text(value='ПКОП')
-    pkop_scan_cnt = ft.TextField(value=SCAN_CNT)
-    pkop_img_cnt = ft.TextField(value=IMG_CNT)
+    pkop = ft.Text(value='Поясничные позвонки')
+    pkop_scan_cnt = ft.TextField(label=SCAN_CNT)
+    pkop_img_cnt = ft.TextField(label=IMG_CNT)
 
     ribs = ft.Text(value='Рёбра и грудина')
-    ribs_scan_cnt = ft.TextField(value=SCAN_CNT)
-    ribs_img_cnt = ft.TextField(value=IMG_CNT)
+    ribs_scan_cnt = ft.TextField(label=SCAN_CNT)
+    ribs_img_cnt = ft.TextField(label=IMG_CNT)
 
     teeth = ft.Text(value='Зубы')
-    teeth_scan_cnt = ft.TextField(value=SCAN_CNT)
-    teeth_img_cnt = ft.TextField(value=IMG_CNT)
+    teeth_scan_cnt = ft.TextField(label=SCAN_CNT)
+    teeth_img_cnt = ft.TextField(label=IMG_CNT)
 
     jaw = ft.Text(value='Челюстей')
-    jaw_scan_cnt = ft.TextField(value=SCAN_CNT)
-    jaw_img_cnt = ft.TextField(value=IMG_CNT)
+    jaw_scan_cnt = ft.TextField(label=SCAN_CNT)
+    jaw_img_cnt = ft.TextField(label=IMG_CNT)
 
     ppn = ft.Text(value='Околоносовых пазух')
-    ppn_scan_cnt = ft.TextField(value=SCAN_CNT)
-    ppn_img_cnt = ft.TextField(value=IMG_CNT)
+    ppn_scan_cnt = ft.TextField(label=SCAN_CNT)
+    ppn_img_cnt = ft.TextField(label=IMG_CNT)
 
     scull = ft.Text(value='Череп')
-    scull_scan_cnt = ft.TextField(value=SCAN_CNT)
-    scull_img_cnt = ft.TextField(value=IMG_CNT)
+    scull_scan_cnt = ft.TextField(label=SCAN_CNT)
+    scull_img_cnt = ft.TextField(label=IMG_CNT)
 
-    abdomen = ft.Text(value='ОБП')
-    abdomen_scan_cnt = ft.TextField(value=SCAN_CNT)
-    abdomen_img_cnt = ft.TextField(value=IMG_CNT)
+    abdomen = ft.Text(value='Брюшная полость')
+    abdomen_scan_cnt = ft.TextField(label=SCAN_CNT)
+    abdomen_img_cnt = ft.TextField(label=IMG_CNT)
 
-    values = [
+    values = [       # трансформировать в словарь
         [chest, chest_scan_cnt, chest_img_cnt],
         [limbs, limbs_scan_cnt, limbs_img_cnt],
         [pelvis, pelvis_scan_cnt, pelvis_img_cnt],
@@ -92,8 +98,7 @@ def main(page: ft.Page):
             )
         )
 
-    page.add(ft.Row([ft.ElevatedButton('Сохранить')],
-                    alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Row([save_btn], alignment=ft.MainAxisAlignment.CENTER))
 
 
 ft.app(target=main)
--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import openpyxl
 from openpyxl.styles import Alignment
@@ -6,56 +7,72 @@ from openpyxl.styles import Alignment
 file_path = 'отчёт.xlsx'
 
 
-if not os.path.exists(file_path):
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.column_dimensions['A'].width = 40
-    sheet.column_dimensions['B'].width = 20
-    sheet.column_dimensions['C'].width = 20
+def create_table():
+    """Создаёт шаблон таблицы отчёта"""
+    if not os.path.exists(file_path):
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.column_dimensions['A'].width = 40
+        sheet.column_dimensions['B'].width = 20
+        sheet.column_dimensions['C'].width = 20
 
-    cell_names = [
-        'Наименование',
-        'Всего',
-        'ОГК',
-        'Костно-мышечной системы',
-        'Конечности',
-        'Таза и тазобедренных суставов',
-        'Шейные позвонки',
-        'Грудные позвонки',
-        'Поясничные позвонки',
-        'Рёбра и грудина',
-        'Черепа и челюстно-лицевой области',
-        'Зубы',
-        'Челюстей',
-        'Околоносовых пазух',
-        'Череп'
-    ]
+        cell_names = [
+            'Наименование',
+            'Всего',
+            'ОГК',
+            'Костно-мышечной системы',
+            'Конечности',
+            'Таза и тазобедренных суставов',
+            'Шейные позвонки',
+            'Грудные позвонки',
+            'Поясничные позвонки',
+            'Рёбра и грудина',
+            'Черепа и челюстно-лицевой области',
+            'Зубы',
+            'Челюстей',
+            'Околоносовых пазух',
+            'Череп',
+            'Брюшная полость',
+            '',
+            'Сохранено: '
+        ]
 
-    cell = sheet['A1']
-    cell.value = 'Количество процедур по исследованиям'
+        start = 5
+        for value in cell_names:
+            cell = sheet['A' + str(start)]
+            cell.value = value
+            start += 1
 
-    start = 5
-    for value in cell_names:
-        cell = sheet['A' + str(start)]
-        cell.value = value
-        start += 1
+        cell_values = {
+            'A1': 'Количество процедур по исследованиям',
+            'B4': 'Всего исследований',
+            'C4': 'Всего снимков',
+            'B5': '=B7 + B8 + B15',
+            'C5': '=C7 + C8 + C15',
+            'B8': '=B9 + B14',
+            'C8': '=C9 + C14',
+            'B15': '=SUM(B16:B20)',
+            'C15': '=SUM(C16:C20)',
+            'B22': f'{datetime.now().strftime("%d-%m-%y %H:%M")}'
+        }
 
-    cell_values = {
-        'B4': 'Всего исследований',
-        'C4': 'Всего снимков',
-        'B5': '=B7 + B8 + B15',
-        'C5': '=C7 + C8 + C15',
-        'B8': '=B9 + B14',
-        'C8': '=C9 + C14',
-        'B15': '=SUM(B16:B19)',
-        'C15': '=SUM(C16:C19)'
-    }
+        for cell, formula in cell_values.items():
+            sheet[cell].value = formula
 
-    for cell, formula in cell_values.items():
-        sheet[cell].value = formula
+        for row in sheet.iter_rows(min_row=5, max_row=20, min_col=2,
+                                   max_col=3):
+            for cell in row:
+                if cell.value is None:
+                    cell.value = 0
 
-    for col in ['B', 'C']:
-        for cell in sheet[col]:
-            cell.alignment = Alignment(horizontal='center', vertical='center')
+        for col in ['B', 'C']:
+            for cell in sheet[col]:
+                cell.alignment = Alignment(horizontal='center',
+                                           vertical='center')
 
-    wb.save(file_path)
+        wb.save(file_path)
+
+
+def modify_table(values: dict):
+    """Внесение изменений в таблицу"""
+    pass
