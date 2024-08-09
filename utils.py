3898c9be0b1b87d694824dd19453548e78c0c33d
Vladimir_T <lobster2@mail.ru>
2024-08-09 13:12:51 +0300

    Доработана валидация переданных полей

--- utils.py
+++ utils.py
@@ -5,12 +5,40 @@ from fields import values
 from tables import file_path, CELL_VALUES
 
 
+def show_info(info, page):
+    page.snack_bar = ft.SnackBar(ft.Text(f'{info}'))
+    page.snack_bar.open = True
+    page.update()
+
+
 def clear_fields(e, page):
     """Очистить поля"""
     for v in values:
         v[1].value = ''
         v[2].value = ''
     page.update()
+
+
+def validate_data(month, page):
+    if month is None:
+        show_info('Выберите месяц', page)
+        return False
+    for v in values:
+        scan = v[1].value
+        img = v[2].value
+
+        if scan and img:
+            try:
+                int(scan)
+                int(img)
+            except ValueError:
+                show_info('Значение должно быть целым числом', page)
+                return False
+
+        if (scan and not img) or (img and not scan):
+            show_info('Введите количество исследований и снимков', page)
+            return False
+    return True
 
 
 def update_table_values(action, month):
@@ -64,16 +92,14 @@ def update_table_values(action, month):
 
 def add_to_table_values(e, page, month):
     """Прибавить переданные значения к таблице"""
-    page.snack_bar = ft.SnackBar(ft.Text('Сохранено'))
-    page.snack_bar.open = True
-    update_table_values(action='add', month=month)
-    page.update()
+    if validate_data(month=month, page=page):
+        update_table_values(action='add', month=month)
+        show_info('Значения добавлены', page)
 
 
 def rewrite_table_values(e, page, month):
     """Внести новые значения в таблицу, старые удаляются"""
-    page.snack_bar = ft.SnackBar(ft.Text('Сохранено'))
-    page.snack_bar.open = True
+    if validate_data(month=month, page=page):
 
-    update_table_values(action='rewrite', month=month)
-    page.update()
+        update_table_values(action='rewrite', month=month)
+        show_info('Значения обновлены', page)
